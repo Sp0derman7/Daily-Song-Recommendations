0983c903cbb1813300af8193a0d254499cb77f6a
Jongh <w.m.c.d.jongh@student.tue.nl>
2024-07-30 19:50:11 +0200

    added timezone to now() function and fixed none error in song_now in WebBackEnd

--- WebBackEnd.py
+++ WebBackEnd.py
@@ -1,6 +1,12 @@
 import sqlite3
 from flask import Flask, render_template
 from datetime import datetime
+from os import path
+import pytz
+
+ROOT = path.dirname(path.realpath(__file__))
+
+DATABASE = r"C:\Users\20234795\OneDrive\Daily-Song-Recommendations\Songs.db"
 
 app = Flask(__name__, template_folder='html')
 
@@ -8,20 +14,20 @@ app = Flask(__name__, template_folder='html')
 def song_now(current_date, current_time):
     conn = None
     try:
-        conn = sqlite3.connect('Songs.db')
+        conn = sqlite3.connect(DATABASE)
         c = conn.cursor()
 
-        c.execute("""SELECT Title, Artist, Time 
-        FROM Songs 
-        WHERE Date <= ? AND Time <= ? 
-        ORDER BY Date DESC, Time DESC LIMIT 1;""", (current_date, current_time))
+        c.execute("""SELECT Title, Artist, Time
+                     FROM Songs
+                     WHERE Date <= ? AND Time <= ?
+                     ORDER BY Date DESC, Time DESC LIMIT 1;""", (current_date, current_time))
         row = c.fetchone()
 
         if not row:
-            c.execute("""SELECT Title, Artist, Time 
-                          FROM Songs 
-                          WHERE Date < ? 
-                          ORDER BY Date DESC, Time DESC LIMIT 1;""", current_date)
+            c.execute("""SELECT Title, Artist, Time
+                         FROM Songs
+                         WHERE Date < ?
+                         ORDER BY Date DESC, Time DESC LIMIT 1;""", (current_date,))
             row = c.fetchone()
 
         return row if row else (None, None, None)
@@ -36,7 +42,7 @@ def song_now(current_date, current_time):
 def time_till_next_song(current_date, current_time):
     conn = None
     try:
-        conn = sqlite3.connect('Songs.db')
+        conn = sqlite3.connect(DATABASE)
         c = conn.cursor()
 
         c.execute("""SELECT Date, Time
@@ -57,7 +63,6 @@ def time_till_next_song(current_date, current_time):
             next_song_date_time = datetime.strptime(f"{next_song[0]} {next_song[1]}", "%Y-%m-%d %H:%M")
             current_date_time = datetime.strptime(f"{current_date} {current_time}", "%Y-%m-%d %H:%M")
             time_difference = next_song_date_time - current_date_time
-            print(next_song_date_time)
             total_minutes = time_difference.total_seconds() / 60
             hours = int(total_minutes // 60)
             minutes = int(total_minutes % 60)
@@ -74,23 +79,10 @@ def time_till_next_song(current_date, current_time):
             conn.close()
 
 
-@app.route("/")
-def index():
-    current_date = "2024-07-27"
-    current_time = "14:00"
-    song_title, artist_name, time = song_now(current_date, current_time)
-    hours, minutes = time_till_next_song(current_date, current_time)
-    return render_template('index.html',
-                           song_title=song_title,
-                           artist_name=artist_name,
-                           hour=hours,
-                           min=minutes)
-
-
 def get_past_songs(current_date, current_time):
     conn = None
     try:
-        conn = sqlite3.connect('Songs.db')
+        conn = sqlite3.connect(DATABASE)
         c = conn.cursor()
 
         c.execute("""SELECT Title, Artist, Date
@@ -107,6 +99,19 @@ def get_past_songs(current_date, current_time):
             conn.close()
 
 
+@app.route("/")
+def index():
+    current_date = datetime.now(pytz.timezone('Europe/Amsterdam')).strftime("%Y-%m-%d")
+    current_time = datetime.now(pytz.timezone('Europe/Amsterdam')).strftime("%H:%M")
+    song_title, artist_name, time = song_now(current_date, current_time)
+    hours, minutes = time_till_next_song(current_date, current_time)
+    return render_template('index.html',
+                           song_title=song_title,
+                           artist_name=artist_name,
+                           hour=hours,
+                           min=minutes)
+
+
 @app.route("/about")
 def about():
     return render_template('About_this_site.html')
@@ -114,8 +119,8 @@ def about():
 
 @app.route("/history")
 def history():
-    current_date = "2024-07-27"
-    current_time = "14:00"
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    current_time = datetime.now().strftime("%H:%M")
     past_songs = get_past_songs(current_date, current_time)
     return render_template('History.html', past_songs=past_songs)
 
